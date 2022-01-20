c4c4741c5088d54d641f078cf885bf876aa03c2b
frla1901 <frla1901@student.miun.se>
2022-01-20 23:18:51 +0100

    added form and adjusted page

--- src/js/button.js
+++ src/js/button.js
@@ -1,4 +1,9 @@
-// funktion som skickar vidare till en annan url än tidigare. => mitt Github repo för detta moment
+// funktion som skickar vidare till en annan url än tidigare. => mitt Github repo för del 1 av detta moment
 function buttonFunction() {
+    location.replace("https://github.com/frla1901/courses_api.git")
+}
+
+function buttonFunction2() {
     location.replace("https://github.com/frla1901/educationApp.git")
 }
+
